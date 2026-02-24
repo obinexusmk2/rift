970c30ec061e7d6eb1b7cb953200f7f655678f85
Nnamdi Michael Okpala <obinexusmk2@proton.me>
2026-02-24 22:23:44 +0000

    rift

---
+++ build/CMakeFiles/rift_pipeline.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/OBINexus/Workspace/rift/rift/rift/rift-000/rift_000_tokenizer.c" "CMakeFiles/rift_pipeline.dir/rift-000/rift_000_tokenizer.c.o" "gcc" "CMakeFiles/rift_pipeline.dir/rift-000/rift_000_tokenizer.c.o.d"
+  "/mnt/c/Users/OBINexus/Workspace/rift/rift/rift/rift-001/rift_001_process.c" "CMakeFiles/rift_pipeline.dir/rift-001/rift_001_process.c.o" "gcc" "CMakeFiles/rift_pipeline.dir/rift-001/rift_001_process.c.o.d"
+  "/mnt/c/Users/OBINexus/Workspace/rift/rift/rift/rift-333/rift_333_ast.c" "CMakeFiles/rift_pipeline.dir/rift-333/rift_333_ast.c.o" "gcc" "CMakeFiles/rift_pipeline.dir/rift-333/rift_333_ast.c.o.d"
+  "/mnt/c/Users/OBINexus/Workspace/rift/rift/rift/rift-444/rift_444_target.c" "CMakeFiles/rift_pipeline.dir/rift-444/rift_444_target.c.o" "gcc" "CMakeFiles/rift_pipeline.dir/rift-444/rift_444_target.c.o.d"
+  "/mnt/c/Users/OBINexus/Workspace/rift/rift/rift/rift-555/rift_555_bridge.c" "CMakeFiles/rift_pipeline.dir/rift-555/rift_555_bridge.c.o" "gcc" "CMakeFiles/rift_pipeline.dir/rift-555/rift_555_bridge.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/OBINexus/Workspace/rift/rift/rift
+# Build directory: /mnt/c/Users/OBINexus/Workspace/rift/rift/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(pipeline_basic "/mnt/c/Users/OBINexus/Workspace/rift/rift/build/rift_tests" "basic")
+set_tests_properties(pipeline_basic PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/OBINexus/Workspace/rift/rift/rift/CMakeLists.txt;129;add_test;/mnt/c/Users/OBINexus/Workspace/rift/rift/rift/CMakeLists.txt;0;")
+add_test(tokenizer_test "/mnt/c/Users/OBINexus/Workspace/rift/rift/build/rift_tests" "tokenizer")
+set_tests_properties(tokenizer_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/OBINexus/Workspace/rift/rift/rift/CMakeLists.txt;130;add_test;/mnt/c/Users/OBINexus/Workspace/rift/rift/rift/CMakeLists.txt;0;")
+add_test(nsigii_codec_test "/mnt/c/Users/OBINexus/Workspace/rift/rift/build/rift_tests" "nsigii")
+set_tests_properties(nsigii_codec_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/OBINexus/Workspace/rift/rift/rift/CMakeLists.txt;131;add_test;/mnt/c/Users/OBINexus/Workspace/rift/rift/rift/CMakeLists.txt;0;")
+add_test(riftbridge_test "/mnt/c/Users/OBINexus/Workspace/rift/rift/build/rift_tests" "riftbridge")
+set_tests_properties(riftbridge_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/OBINexus/Workspace/rift/rift/rift/CMakeLists.txt;132;add_test;/mnt/c/Users/OBINexus/Workspace/rift/rift/rift/CMakeLists.txt;0;")
